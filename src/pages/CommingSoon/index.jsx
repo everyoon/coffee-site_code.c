dcb3c6e6754bb3d1f965d07116a05d1892f3af23
everyoon <jy.k7377@gmail.com>
2025-10-15 23:20:56 +0900

    commingsoon 페이지 완료

--- src/pages/CommingSoon/index.jsx
+++ src/pages/CommingSoon/index.jsx
@@ -1,3 +1,25 @@
+import styles from './CommingSoon.module.css';
+import Img from '@assets/images/Commingsoon_img.png';
+import Button from '../../components/common/Button';
+
 export default function ComingSoon() {
-  return <div>준비중</div>;
+  return (
+    <>
+      <div className={`container ${styles.container}`}>
+        <div className={styles.title}>
+          해당 페이지는 현재 준비 중입니다.
+          <br />
+          조금만 기다려주시면 더 나은 모습으로 찾아뵙겠습니다.
+        </div>
+        <div className={styles.img}>
+          <img src={Img} alt="공사중인 이미지" />
+        </div>
+        <div className={styles.buttonInner}>
+          <Button className={styles.button} to="/" size="large" color="primary">
+            메인페이지로 돌아가기
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
