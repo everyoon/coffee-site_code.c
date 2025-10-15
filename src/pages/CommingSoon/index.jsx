836665b034c8e89e6e460559b2a0ae6ec58535f9
everyoon <jy.k7377@gmail.com>
2025-10-15 23:28:04 +0900

    commingsoon css 수정

--- src/pages/CommingSoon/index.jsx
+++ src/pages/CommingSoon/index.jsx
@@ -5,19 +5,21 @@ import Button from '../../components/common/Button';
 export default function ComingSoon() {
   return (
     <>
-      <div className={`container ${styles.container}`}>
-        <div className={styles.title}>
-          해당 페이지는 현재 준비 중입니다.
-          <br />
-          조금만 기다려주시면 더 나은 모습으로 찾아뵙겠습니다.
-        </div>
-        <div className={styles.img}>
-          <img src={Img} alt="공사중인 이미지" />
-        </div>
-        <div className={styles.buttonInner}>
-          <Button className={styles.button} to="/" size="large" color="primary">
-            메인페이지로 돌아가기
-          </Button>
+      <div className={styles.wrap}>
+        <div className={`container ${styles.container}`}>
+          <div className={styles.title}>
+            해당 페이지는 현재 준비 중입니다.
+            <br />
+            조금만 기다려주시면 더 나은 모습으로 찾아뵙겠습니다.
+          </div>
+          <div className={styles.img}>
+            <img src={Img} alt="공사중인 이미지" />
+          </div>
+          <div className={styles.buttonInner}>
+            <Button className={styles.button} to="/" size="large" color="primary">
+              메인페이지로 돌아가기
+            </Button>
+          </div>
         </div>
       </div>
     </>
